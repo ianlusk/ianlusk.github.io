d3c16ae120ce4145c29aec75938efe7037842775
Ian Lusk <ianlusk@users.noreply.github.com>
2019-08-11 13:59:19 -0400

    2019 Updates, Adding Clipboard.js
    Updated core JS file to support new clipboard functionality. Added Clipboard.js to support copy-to-clickboard functionality of email clone button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 
+	// Initialize the Copy button next to my email address
+	new ClipboardJS('#copyMyEmail'); 
+
+	$('#copyMyEmail').on('click',function(){
+		$(this).addClass('copied');
+	});
+
+	// Toggle sections when H2's are clicked.
 	$('h2').on('click',function(){
-		if ($(window).width() < 800) {
-			$(this).next('.info-list').slideToggle();
-			$(this).toggleClass('active').toggleClass('inactive');
-		}
+		$(this).next('.info-list').slideToggle();
+		$(this).toggleClass('active').toggleClass('inactive');
 	});
 
 });
